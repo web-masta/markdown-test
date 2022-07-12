e5eb4d1f83abbf7019a6846bd48f5dfe37c2d130
Ivan Veselov <ivan.webmasta@gmail.com>
2022-07-12 11:40:48 +0300

    refactor(markdown controller): refactor of controller name

--- src/Controllers/MarkdownController.php
+++ src/Controllers/MarkdownController.php
@@ -5,14 +5,14 @@
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
 
-    class IndexController extends AbstractController
+    class MarkdownController extends AbstractController
     {
         #[Route('/markdown', name: 'markdown_index')]
         public function index(): Response
         {
             return $this->json([
                 'status' => 'success',
-                'message' => 'Welcome to Markdown IndexController!',
+                'message' => 'Welcome to Markdown MarkdownController!',
                 'rand' => rand(1, 100),
             ]);
         }
